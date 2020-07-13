68322adf1110c600cff60c8615a563a9d6fdd8aa
Federico <feka7@hotmail.it>
2020-07-13 16:16:33 +0200

    final

--- api/utils.py
+++ api/utils.py
@@ -3,8 +3,8 @@ from web3 import Web3
 
 def sendTransaction(message):
     w3 = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/5471e52a04844eca9fcf39ee5791afc3'))
-    address = '0x2e7289F770596d41131f5C46869FE5aa16C06f66'
-    privateKey = '0xa082c4d8429d1c748be87ed544bfc7dfa8be5514b0f3cd4f4737694280a3589d'
+    address = '0x------'
+    privateKey = '0x-----'
     nonce = w3.eth.getTransactionCount(address)
     gasPrice = w3.eth.gasPrice
     value = w3.toWei(0, 'ether')
